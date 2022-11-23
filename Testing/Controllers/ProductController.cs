e64e499d5731122f4687c130c324a2c9ec54a8b2
Michael Hatoum <hatoumm89@gmail.com>
2022-11-23 13:21:59 -0500

    all done

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -52,6 +52,11 @@ namespace Testing.Controllers
             repo.InsertProduct(productToInsert);
             return RedirectToAction("Index");
         }
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+            return RedirectToAction("Index");
+        }
 
     }
 }
